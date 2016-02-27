b2770b45df80117c3c9b878040748a38b819f908
Alvaro Martinez <Al@nbp-155-27.nbp.ruw.rutgers.edu>
2016-02-27 12:10:23 -0500

    added font

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,45 @@
-alert("Hello Al!")
+var triviaGame = {
+
+	newGame : {
+			 wins: 0,
+			 loses: 0,
+			 currentQuestion:[]
+
+
+	},
+	questionOne: {
+
+			question: "What is the closest planet to the sun? ",
+			reply1 : "The moon",
+			reply2 : "Mars ",
+			reply3 : "Jupiter",
+			reply4 : "Mercury",
+			answer : "Mercury"
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
